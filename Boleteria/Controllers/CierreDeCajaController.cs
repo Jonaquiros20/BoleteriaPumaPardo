ba1c597e60c186395f56654df01147fead233186
Jonaquiros20 <154646778+Jonaquiros20@users.noreply.github.com>
2024-11-21 07:53:11 -0600

    mejoras varias

---
+++ Boleteria/Controllers/CierreDeCajaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Boleteria.Controllers
+{
+    public class CierreDeCajaController : Controller
+    {
+        public IActionResult CierreDeCaja()
+        {
+            return View();
+        }
+    }
+}
